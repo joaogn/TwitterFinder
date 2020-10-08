0d2a57d4d6cca3ee9048707baaac673acb0d2a86
João Graça <joao.g.n91@gmail.com>
2020-10-07 23:03:26 -0300

    fix: Now web folder show on github

---
+++ Web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <div className="App">
+        <h1>Hello Word</h1>
+      </div>
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default App;
