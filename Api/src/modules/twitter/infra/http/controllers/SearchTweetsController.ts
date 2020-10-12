6ebac2028f8cf44355cc7015f064d93aea5b57ef
João Graça <joao.g.n91@gmail.com>
2020-10-12 12:31:58 -0300

    fix: the twitter query founded wrong result

--- Api/src/modules/twitter/infra/http/controllers/SearchTweetsController.ts
+++ Api/src/modules/twitter/infra/http/controllers/SearchTweetsController.ts
@@ -4,9 +4,9 @@ import { container } from 'tsyringe';
 
 export default class SearchTweetsController {
   public async index(request: Request, response: Response): Promise<Response> {
-    const { q } = request.body;
+    const { q } = request.query;
     const searchTweets = container.resolve(SearchTweetsService);
-    const tweets = await searchTweets.execute(q);
+    const tweets = await searchTweets.execute(String(q));
     return response.json(tweets);
   }
 }
